e9352f9431d900520d8ebb93c512cc0cdcb650c6
AndreyZhirkin <partner101@inbox.ru>
2023-04-01 14:15:03 +0300

    KS-1-1 first task - done

---
+++ src/main/kotlin/lesson_1/lesson1_task_1.kt
@@ -0,0 +1,9 @@
+package lesson_1
+
+fun main (){
+    val amountOrders = 75
+    val thankPage = "Спасибо за покупку!"
+    val rectangleHeight: Int
+    val rectangleWidth: Int
+
+}
--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++
@@ -1,5 +0,0 @@
-package lesson_1
-
-fun main (){
-    println("Что-то необычное, опа")
-}
