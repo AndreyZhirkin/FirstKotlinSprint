5be1bfce5b676fb3721f888f2b48701a579da9da
AndreyZhirkin <partner101@inbox.ru>
2023-04-01 16:36:59 +0300

    KS-1-2 first task - done

---
+++ src/main/kotlin/lesson_1/lesson1_task_2.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main (){
+    val amountOrders = 75
+    val thankPage = "Cпасибо за покупку!"
+    val rectangleHeight: Int = 10
+    val rectangleWidth: Int = 8
+    println("$amountOrders \n$thankPage \n$rectangleHeight, $rectangleWidth")
+    var quantityEmployees = 2000
+    // println("$quantityEmployees")
+     quantityEmployees = 1999
+    println("$quantityEmployees")
+}
